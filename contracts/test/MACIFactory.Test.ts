ea755f82833187b83a73c0d1cf930a01e1da28d2
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2020-12-10 18:53:22 +0900

    Added MACIFactory.Test.ts

---
+++ contracts/test/MACIFactory.Test.ts
@@ -0,0 +1,44 @@
+const {revertSnapshot, takeSnapshot} = require('./TestUtil')
+
+const MACIFactory = artifacts.require('MACIFactory')
+const BatchUpdateStateTreeVerifierSmall  = artifacts.require('BatchUpdateStateTreeVerifierSmall')
+
+contract('MACIFactory', (accounts) => {
+  let instance
+  let snapshotId
+  before(async () => {
+	instance = await MACIFactory.deployed()
+	batchUstVerifierInstance = await BatchUpdateStateTreeVerifierSmall.deployed()
+	snapshotId = await takeSnapshot()
+  })
+  
+  describe('initialize', () => {
+	it('should correctly initialized', async () => {
+	  // TODO: check more variables
+	  
+	  const batchUstVerifierAddress = await instance.batchUstVerifier()
+	  const votingDuration = await instance.votingDuration()
+	  assert.equal(batchUstVerifierAddress, batchUstVerifierInstance.address)
+	  assert.equal(votingDuration, 604800)
+	})
+
+	// TODO
+	// it('should be able to set MACI parameters', async () => {
+	// 	  
+	// 	})
+
+	// TODO
+	// it('should be able to deploy MACI', async () => {
+	// 	  
+	// 	})
+	// it('should revert if non owner try to deploy MACI', async () => {
+	// 	  
+	// 	})
+  })
+
+  afterEach(async () => {
+    await revertSnapshot(snapshotId.result)
+    // eslint-disable-next-line require-atomic-updates
+    snapshotId = await takeSnapshot()
+  })
+})
