b5e54cc4ede46b292773e4ab3bd4dccdde4b08a4
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2021-01-12 16:57:30 +0900

    Changed contract license from MIT to GPL-3.0

--- contracts/contracts/MerkleTreeWithHistory.sol
+++ contracts/contracts/MerkleTreeWithHistory.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 // heavily inspired by https://github.com/tornadocash/tornado-core/blob/master/contracts/MerkleTreeWithHistory.sol
 pragma solidity ^0.6.12;
 
