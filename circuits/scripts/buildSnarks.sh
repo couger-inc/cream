6203d4362be09431809af466681d9ec031367238
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2021-01-29 13:56:05 +0900

    Fixed sed command for OS agnostic pattern

--- circuits/scripts/buildSnarks.sh
+++ circuits/scripts/buildSnarks.sh
@@ -11,17 +11,23 @@ REPLACE_TARGET_FILE=$PROJECT_DIR/node_modules/maci-circuits/build/index.js
 # Need to overwrite zkutilPath variable due to mocha test
 # check if build file exists
 if [ ! -f $REPLACE_TARGET_FILE ]; then
-	echo "$REPLACE_TARGET_FILE not exist. exiting..."
-	exit
+    echo "$REPLACE_TARGET_FILE not exist. exiting..."
+    exit
 else
-	cp $REPLACE_TARGET_FILE $REPLACE_TARGET_FILE.bk
+    cp $REPLACE_TARGET_FILE $REPLACE_TARGET_FILE.bk
 fi
 
-sed -i ” 's/maci_config_1.config.zkutil_bin/"~\/.cargo\/bin\/zkutil"/g' $REPLACE_TARGET_FILE
+case "$OSTYPE" in
+  darwin*)  arg="”" ;;
+  linux*)   arg="" ;;
+  *)        arg="" ;;
+esac
+
+sed -i $arg 's/maci_config_1.config.zkutil_bin/"~\/.cargo\/bin\/zkutil"/g' $REPLACE_TARGET_FILE
 
 # check if build directory exists
 if [ ! -e $BUILD_DIR ]; then
-  mkdir -p $BUILD_DIR
+    mkdir -p $BUILD_DIR
 fi
 
 # build snarks
