8a61964cbe6fff3f50a3173a6299cd3d56250ab0
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2021-09-13 13:14:18 +0900

    Addec truffle migration for docker

--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -1,11 +1,17 @@
 module.exports = {
   networks: {
     development: {
-      network_id: "*",
-      gas: 15000000,
       host: "localhost",
-      port: 8545
-    }
+      port: 8545,
+      network_id: "*",
+      gas: 15000000
+    },
+    dockerGanache: {
+      host: '172.17.0.2',
+      port: 8545,
+      network_id: '*',
+      gas: 15000000,
+    },
   },
   compilers: {
     solc: {
